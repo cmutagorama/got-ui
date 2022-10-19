067b6314ead0fb24bc27a66202d80f1e1109f699
Charles Mutagorama <cmutagorama@gmail.com>
2022-10-20 01:43:02 +0200

    added tests for routing

---
+++ src/__tests__/routes.test.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { create } from "react-test-renderer";
+import {
+	MemoryRouter,
+	Routes,
+	Route,
+} from "react-router-dom";
+import Home from "../pages/home";
+import House from "../pages/house";
+
+describe("Routes", () => {
+	let consoleWarn: jest.SpyInstance;
+  let consoleError: jest.SpyInstance;
+  beforeEach(() => {
+    consoleWarn = jest.spyOn(console, "warn").mockImplementation(() => {});
+    consoleError = jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleWarn.mockRestore();
+    consoleError.mockRestore();
+	});
+
+  it("renders home", () => {
+    let renderer = create(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<Home />}>
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+	});
+	
+	it("renders house details", () => {
+    let renderer = create(
+      <MemoryRouter initialEntries={["/house/1"]}>
+        <Routes>
+          <Route path="/" element={<House />}>
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+  });
+});
