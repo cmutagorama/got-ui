79a137e8693d63f108c29502afba66cb695e998f
Charles Mutagorama <cmutagorama@gmail.com>
2022-10-20 23:47:35 +0200

    implemented about page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { Route, Routes } from 'react-router-dom';
+import { About } from '../pages/about';
 import Home from '../pages/home';
 import House from '../pages/house';
 import { NoMatch } from '../pages/nomatch';
@@ -8,8 +9,9 @@ const AppRoutes: FC = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
+			<Route path='/about' element={<About />} />
 			<Route path='house/:id' element={<House />} />
-			<Route path="*" element={<NoMatch />} />
+			<Route path='*' element={<NoMatch />} />
 		</Routes>
 	)
 }
