04c892d8605a524f4cf8c97f3747927f1c945f90
Charles Mutagorama <cmutagorama@gmail.com>
2022-10-21 13:26:38 +0200

    commented out characters page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Books from '../pages/books';
-import Characters from '../pages/characters';
+// import Characters from '../pages/characters';
 import Home from '../pages/home';
 import House from '../pages/house';
 import { NoMatch } from '../pages/nomatch';
@@ -10,7 +10,7 @@ const AppRoutes: FC = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
-			<Route path='/characters' element={<Characters />} />
+			{/* <Route path='/characters' element={<Characters />} /> */}
 			<Route path='/books' element={<Books />} />
 			<Route path='/house/:id' element={<House />} />
 			<Route path='*' element={<NoMatch />} />
