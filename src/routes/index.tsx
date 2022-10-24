6a41bfcefbe2f9f4d425914c0cf1ca2c8f879c58
Charles Mutagorama <cmutagorama@gmail.com>
2022-10-24 12:01:04 +0200

    added houses route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,17 @@ import Books from '../pages/books';
 import Characters from '../pages/characters';
 import Home from '../pages/home';
 import House from '../pages/house';
+import Houses from '../pages/houses';
 import { NoMatch } from '../pages/nomatch';
 
 const AppRoutes: FC = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
+			<Route path='/houses' element={<Houses />} />
+			<Route path='/house/:id' element={<House />} />
 			<Route path='/characters' element={<Characters />} />
 			<Route path='/books' element={<Books />} />
-			<Route path='/house/:id' element={<House />} />
 			<Route path='*' element={<NoMatch />} />
 		</Routes>
 	)
