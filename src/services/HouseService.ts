b8482fbcd79a62fa93a13374f8154dc28cc38c99
Charles Mutagorama <cmutagorama@gmail.com>
2022-10-25 12:05:27 +0200

    added name parameter

--- src/services/HouseService.ts
+++ src/services/HouseService.ts
@@ -1,8 +1,9 @@
 import http from "../http-common";
 import { IHouse } from "../types/House";
 
-const getAll = (page: number = 1, pageSize: number = 12) => {
-	return http.get<IHouse[]>(`/api/houses?page=${page}&pageSize=${pageSize}`);
+const getAll = (page: number = 1, pageSize: number = 12, name?: string) => {
+	const URL = !!name ? `/api/houses?page=${page}&pageSize=${pageSize}&name=House ${name}` : `/api/houses?page=${page}&pageSize=${pageSize}`;
+	return http.get<IHouse[]>(URL);
 }
 
 const findHouse = (id: string) => {
